52f101343175cd712c8aad1350093daec761911d
parvezislam <parvezislam45@gmail.com>
2022-04-17 01:43:50 +0600

    make carosell banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Banner from './Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <h1>David Machine Service Center</h1>
+      <Banner></Banner>
+      <h1>mechine service</h1>
     </div>
   );
 }
