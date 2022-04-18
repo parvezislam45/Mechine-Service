c91bfd0885b7032ba7a1428c77ed1a12c3454304
parvezislam <parvezislam45@gmail.com>
2022-04-18 13:49:33 +0600

    add about me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,26 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <div>
+            <div className="container mx-auto">
+                <Card >
+                    <Card.Img className=' image w-25 rounded mx-auto d-block' variant="top" src={img} />
+                    <Card.Body>
+                        <h1>Parvez Islam</h1>
+                        <h4>Mechanical Engineer </h4>
+                        <h4>Graduated</h4>
+                        <h5>Institute :International University Of Business Agriculture & Technology</h5>
+                        <h5>Address : Home:152/1,Bilashdi</h5>
+                        <h5>City : Narsingdi</h5>
+                        <h5>Hobby : Play Cricket,Football,Carrome,Traveling</h5>
+
+                        <Card.Text>
+                            <h3>Something About Me</h3>
+                        <h5>I am Parvez Islam. I was born in Narsingdi. My father is an established businessman. I grew up in Narsingdi from a young age. Anger is too much but respect for people as human beings does not give money power. State Forward people are not afraid to tell the truth. They are willing to do everything for Islam. I love Islam with my life.i have Love Playing Cricket Since Childhood !!</h5>
+                        </Card.Text>
+                        <h1>Future Goal</h1>
+                        <h5>I want to be a good programmer. I want to do something new. I want to do something for Islam by using cyber.<br></br> I want to lead my life in an honest way. I always hope for Allah's help !! </h5>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
